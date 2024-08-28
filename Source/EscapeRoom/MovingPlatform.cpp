5eebdc61d182724f9130a8ac3a8ab272923bd1e0
LAPILA <lapila3004@gmail.com>
2024-08-28 18:05:32 +0900

    MovePlatform

---
+++ Source/EscapeRoom/MovingPlatform.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsMove) 
+	{
+		//틱당 위치를 받고
+		FVector Location = GetActorLocation();
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+		if (JourneyTravelled > JourneyLength)
+		{
+			FVector Swap = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Swap;
+		}
+		//숫자가 작을때 보호해주는 SafeNormal
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		// 속도 * 초당시간 * 방향
+		Location += Speed * DeltaTime * Direction;
+		//해당 위치를 재설정
+		SetActorLocation(Location);
+	}
+	
+}
---
+++ Source/EscapeRoom/MovingPlatform.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class ESCAPEROOM_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+
+	UPROPERTY(EditAnywhere, meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+	UPROPERTY(EditAnywhere)
+	bool IsMove = true;
+
+private:
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
+
+};
